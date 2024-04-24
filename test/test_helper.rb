e99fb273669a5ea4dec82ac37c256e5f958e93fe
ShoyaUeno1026 <ueno@hab-co.jp>
2024-04-24 08:26:03 +0000

    Finish layout and routes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,9 +7,7 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # 指定のワーカー数でテストを並列実行する
   parallelize(workers: :number_of_processors)
-
   # test/fixtures/*.ymlにあるすべてのfixtureをセットアップする
   fixtures :all
-
-  # （すべてのテストで使うその他のヘルパーメソッドは省略）
+  include ApplicationHelper
 end
